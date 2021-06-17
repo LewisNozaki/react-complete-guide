f5be6cacaf6516ee7642a0b5f3940ec198e6f570
Lewis Nozaki <lhnozaki@outlook.com>
2021-06-16 17:39:19 -1000

    updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-function App() {
+const App = () => {
   return (
     <div>
-      <h2>
-        Let's get started.
-      </h2>
+      <h2>Let's get started.</h2>
+      <p>Hello World</p>
     </div>
   );
 }
