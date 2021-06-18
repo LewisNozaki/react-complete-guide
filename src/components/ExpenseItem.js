c6c55c8640517cd4b7c79fc80394c9129a96192e
Lewis Nozaki <lhnozaki@outlook.com>
2021-06-17 16:42:16 -1000

    updates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
-import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = ({title, amount, date}) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate 
         date={date}
       />
@@ -11,7 +12,7 @@ const ExpenseItem = ({title, amount, date}) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-    </div>
+    </Card>
   )
 } 
 
