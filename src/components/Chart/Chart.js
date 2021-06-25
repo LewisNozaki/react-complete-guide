8efaaac54aace8088759e8456419d32366ece11f
Lewis Nozaki <lhnozaki@outlook.com>
2021-06-24 14:39:22 -1000

    updates

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,6 +3,10 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = ({ dataPoints }) => {
+  const valueArr = dataPoints.map(data => data.value);
+  
+  const totalMax = Math.max(...valueArr);
+
   return (
     <div className="chart">
       {dataPoints.map(data => {
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./ChartBar.css";
 
-const ChartBar = ({}) => {
+const ChartBar = ({ value, maxValue, label }) => {
+  let barFillHeight = "0%";
+
+  if (maxValue > 0) {
+    barFillHeight = Math.round(( value / maxValue ) * 100) + "%";
+  }
+
+  let barFillStyle = {
+    height: barFillHeight
+  }
+  
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill"></div>
+        <div 
+          className="chart-bar__fill" 
+          style={barFillStyle}
+          ></div>
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label">{label}</div>
     </div>
   )
 }
